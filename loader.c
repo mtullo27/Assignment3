5e5dd1e01fd9a3b38bcaff633305b3ebf7abb9ea
mtullo27 <36345087+mtullo27@users.noreply.github.com>
2018-11-29 18:35:26 -0500

    sj

--- loader.c
+++ loader.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-	unsigned char raw_bytes[300];
-	typedef int (*Calc_fptr)(char, int, int);
-	FILE *fp;
-	unsigned int i;
-	int int1 = atoi(argv[1]);
-	int int2 = atoi(argv[3]);
-	char sym = *argv[2];
-	Calc_fptr calculator;
-	if(argc != 4){
-		printf("Ussage %s <filename> <unit> <operation> <unit>\n", argv[0]);
-		exit(-1);
-	}
-	fp = fopen(argv[0], "rb");
-	fread(raw_bytes, sizeof(unsigned char), 92, fp);
-	calculator = (Calc_fptr)raw_bytes;
-	int val = calculator(sym, int1, int2);
-	printf("%i %c %i = %i", int1, argv[2], int2, val);
-	return 0;
+  if(argc != 5){
+    printf("Usage %s <filename> <unit> <operation> <unit>\n", argv[0]);
+    exit(-1);
+  }
+  unsigned char raw_bytes[300];
+  typedef int (*Calc_fptr)(char, int, int);
+  FILE *fp;
+  int int1 = atoi(argv[2]);
+  int int2 = atoi(argv[4]);
+  char sym = *argv[3];
+  Calc_fptr calculator;
+  fp = fopen(argv[1], "rb");
+  fread(raw_bytes, 1, 92, fp);
+  calculator = (Calc_fptr)raw_bytes;
+  int val = calculator(sym, int1, int2);
+  printf("%i %c %i = %i", int1, sym, int2, val);
+  return 0;
 }
---
+++ question3.c
@@ -0,0 +1,9 @@
+typedef struct{
+	unsigned char valid;
+	unsigned char tag;
+	unsigned char block;
+}cache_line;
+int main(){
+	char **chache;
+	for(
+}
