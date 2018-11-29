fa47cad549e36f4795a947b593e580878aeff1a2
mtullo27 <36345087+mtullo27@users.noreply.github.com>
2018-11-29 14:52:46 -0500

    bjbdas

---
+++ loader.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	unsigned char raw_bytes[300];
+	typedef int (*Calc_fptr)(char, int, int);
+	FILE *fp;
+	unsigned int i;
+	int int1 = atoi(argv[1]);
+	int int2 = atoi(argv[3]);
+	Calc_fptr calculator;
+	if(argc != 4){
+		printf("Ussage %s <filename> <unit> <operation> <unit>\n", argv[0]);
+		exit(-1);
+	}
+	fp = fopen(argv[0], "rb");
+	fread(raw_bytes, sizeof(unsigned char), 92, fp);
+	calculator = (Calc_fptr)raw_bytes;
+	printf("%i %c %i = %i", int1, argv[2], int2);
+	return 0;
+}
