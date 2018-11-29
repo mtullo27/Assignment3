8bdc434c186c843e1210ec8eff4610cadbe699c9
mtullo27 <36345087+mtullo27@users.noreply.github.com>
2018-11-29 16:50:33 -0500

    fix

--- loader.c
+++ loader.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]){
 	unsigned int i;
 	int int1 = atoi(argv[1]);
 	int int2 = atoi(argv[3]);
+	char sym = *argv[2];
 	Calc_fptr calculator;
 	if(argc != 4){
 		printf("Ussage %s <filename> <unit> <operation> <unit>\n", argv[0]);
@@ -16,6 +17,7 @@ int main(int argc, char *argv[]){
 	fp = fopen(argv[0], "rb");
 	fread(raw_bytes, sizeof(unsigned char), 92, fp);
 	calculator = (Calc_fptr)raw_bytes;
-	printf("%i %c %i = %i", int1, argv[2], int2);
+	int val = calculator(sym, int1, int2);
+	printf("%i %c %i = %i", int1, argv[2], int2, val);
 	return 0;
 }
